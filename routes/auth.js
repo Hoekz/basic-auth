73bf98d874433467558ec084babcfc3f468e2545
James Hoekzema <james.hoekzema@wwt.com>
2022-05-24 15:49:16 -0500

    basic auth app - JH

---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+const { createUser, createToken, loginUser } = require('../auth');
+
+// create user
+router.post('/', async (req, res) => {
+  const { email, password, name } = req.body;
+
+  const user = await createUser(email, password, name);
+  const token = await createToken(user);
+
+  res.cookie('authorization', token, { httpOnly: true });
+  res.json(user);
+});
+
+// login user
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await loginUser(email, password);
+
+  if (!user) {
+    return res.status(401).json({ message: 'unrecognized email or password' });
+  }
+
+  const token = await createToken(user);
+
+  res.cookie('authorization', token, { httpOnly: true });
+  res.json(user);
+});
+
+module.exports = router;
